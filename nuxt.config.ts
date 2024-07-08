822ece57ec612284a260267fcfde1365bb043e63
svertkatter <alice241102@gmail.com>
2024-07-08 16:35:27 +0900

    style: :lipstick: ファビコンの設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,7 +29,7 @@ export default defineNuxtConfig({
         ],
         link: [
             {
-                rel : 'icon', type: 'image/x-icon', href: '/favicon3.png'
+                rel : 'icon', type: 'image/x-icon', href: '/favicon.ico'
             },
         ],
     },
