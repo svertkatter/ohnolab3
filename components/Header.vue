6ac5bb8ad90463263b9d40b4da255f7a9b721f23
svertkatter <alice241102@gmail.com>
2024-07-19 20:16:56 +0900

    feat: :sparkles: ヘッダーの追加

---
+++ components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <header>
+            <div class="header">
+                <h1>
+                    <NuxtLink to="/"><NuxtImg src="/img/logo/ohnologo-1.svg" alt="大野研究室ロゴ"/></NuxtLink>
+                </h1>
+                <ul class="flex nav">
+                    <li><NuxtLink to="/researches">Researches</NuxtLink></li>
+                    <li><NuxtLink to="/equipments">Equipments</NuxtLink></li>
+                    <li><NuxtLink to="/activites">Activities</NuxtLink></li>
+                    <li><NuxtLink to="/members">Member</NuxtLink></li>
+                    <li><NuxtLink to="/room">Room</NuxtLink></li>
+                    <li><NuxtLink to="/access">Access</NuxtLink></li>
+                </ul>
+            </div>
+        </header>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,41 +1,45 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
-  app: {
-    head: {
-        htmlAttrs: {
-            lang: 'ja',
-            prefix: 'og: http://ogp.me/ns#',
-        },
-        title: '大野研究室｜大分大学',
-        meta: [
-            {
-                charset: 'utf-8',
-            },
-            {
-                name: 'viewport',
-                content: 'width=device-width, initial-scale=1, viewport-fit=cover',
-            },
-            { hid: 'description', name: 'description', content: '大分大学大野研究室のサイト' },
-            { hid: 'og:site_name', property: 'og:site_name', content: '大野研究室｜大分大学' },
-            { hid: 'og:url', property: 'og:url', content: 'https://ohno.eee.oita-u.ac.jp/' },  //絶対パスで指定
-            { hid: 'og:type', property: 'og:type', content: 'website' },
-            { hid: 'og:image', property: 'og:image', content: 'https://ohno.eee.oita-u.ac.jp/picture-ogp.png' },  //絶対パスで指定
-            { hid: 'og:title', property: 'og:title', content: '大野研究室｜大分大学' },
-            { hid: 'og:description', property: 'og:description', content: '大野研究室｜大分大学' },
-            { name: 'twitter:card', content: 'summary_large_image' },
-            { name: 'twitter:image', content: 'https://ohno.eee.oita-u.ac.jp/picture-ogp.png'}
-        ],
-        link: [
-            {
-                rel : 'icon', type: 'image/x-icon', href: '/favicon.ico'
-            },
-        ],
-    },
-},
-  css: [
-    '@/assets/css/reset.css',
-    '@/assets/css/style.css'
-  ]
+    compatibilityDate: '2024-04-03',
+    devtools: { enabled: true },
+
+    app: {
+      head: {
+          htmlAttrs: {
+              lang: 'ja',
+              prefix: 'og: http://ogp.me/ns#',
+          },
+          title: '大野研究室｜大分大学',
+          meta: [
+              {
+                  charset: 'utf-8',
+              },
+              {
+                  name: 'viewport',
+                  content: 'width=device-width, initial-scale=1, viewport-fit=cover',
+              },
+              { hid: 'description', name: 'description', content: '大分大学大野研究室のサイト' },
+              { hid: 'og:site_name', property: 'og:site_name', content: '大野研究室｜大分大学' },
+              { hid: 'og:url', property: 'og:url', content: 'https://ohno.eee.oita-u.ac.jp/' },  //絶対パスで指定
+              { hid: 'og:type', property: 'og:type', content: 'website' },
+              { hid: 'og:image', property: 'og:image', content: 'https://ohno.eee.oita-u.ac.jp/picture-ogp.png' },  //絶対パスで指定
+              { hid: 'og:title', property: 'og:title', content: '大野研究室｜大分大学' },
+              { hid: 'og:description', property: 'og:description', content: '大野研究室｜大分大学' },
+              { name: 'twitter:card', content: 'summary_large_image' },
+              { name: 'twitter:image', content: 'https://ohno.eee.oita-u.ac.jp/picture-ogp.png'}
+          ],
+          link: [
+              {
+                  rel : 'icon', type: 'image/x-icon', href: '/favicon.ico'
+              },
+          ],
+      },
+  },
+
+    css: [
+      '@/assets/css/reset.css',
+      '@/assets/css/style.css'
+    ],
+
+    modules: ["@nuxt/image"]
 })
