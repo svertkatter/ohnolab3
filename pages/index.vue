7c5cab5a287bae8774ef903187ae01a1faca3de1
svertkatter <alice241102@gmail.com>
2024-08-07 13:10:37 +0900

    feat: :sparkles: セクションの追加、画像の追加

--- pages/index.vue
+++ pages/index.vue
@@ -6,5 +6,54 @@
             <p>ナノエレクトロニクスデバイスの設計、試作、評価を行っています。</p>
             <p>「見学」「研究相談」「共同研究」「試料の試作」などございましたらお気軽にご連絡ください。</p>
         </div>
+
+        <div class="section">
+            <div class="section-title">
+                <h2>Researches</h2>
+                <p>研究&業績</p>
+            </div>
+
+            <div class="section-content">
+                <div class="paper-img">
+                    <NuxtImg src="/img/paper/Nanotechnology.webp" alt="paper" />
+                </div>
+            </div>
+        </div>
+
+        <div class="section">
+            <div class="section-title">
+                <h2>Equipments</h2>
+                <p>研究設備</p>
+            </div>
+        </div>
+
+        <div class="section">
+            <div class="section-title">
+                <h2>Activities</h2>
+                <p>活動実績</p>
+            </div>
+        </div>
+
+        <div class="section">
+            <div class="section-title">
+                <h2>Member</h2>
+                <p>メンバー</p>
+            </div>
+        </div>
+
+        <div class="section">
+            <div class="section-title">
+                <h2>Room</h2>
+                <p>研究室の様子</p>
+            </div>
+        </div>
+
+        <div class="section">
+            <div class="section-title">
+                <h2>Access</h2>
+                <p>アクセス</p>
+            </div>
+        </div>
+
     </div>
 </template>
