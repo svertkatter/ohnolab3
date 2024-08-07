fa650611b9131f77531ffa2520c7e6ff7dde1fc7
svertkatter <alice241102@gmail.com>
2024-08-07 17:55:40 +0900

    chore: :pencil2: researchesページの編集

--- pages/researches/index.vue
+++ pages/researches/index.vue
@@ -1,2 +1,10 @@
 <template>
+    <article class="container">
+        <div class="section">
+            <div class="section-title">
+                <h2>Researches</h2>
+                <p>最新の研究&業績</p>
+            </div>
+        </div>
+    </article>
 </template>
