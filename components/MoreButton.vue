bf57d420d8ae783da28a69c653ad6eb485f72a9e
svertkatter <alice241102@gmail.com>
2024-08-07 14:18:41 +0900

    feat: :sparkles: 詳細ボタンの追加

---
+++ components/MoreButton.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="moreButton">
+      <NuxtLink to="/researches" class="buttonLink">
+        <p>More {{ text }}</p>
+        <span class="arrowRight"></span>
+      </NuxtLink>
+    </div>
+  </template>
+  
+  <script setup>
+  import { defineProps } from 'vue';
+  
+  const props = defineProps({
+    text: String
+  });
+  </script>
