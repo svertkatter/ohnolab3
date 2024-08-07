e61dbf5832852bd2f8065af641845170e7421b66
svertkatter <alice241102@gmail.com>
2024-08-07 13:44:19 +0900

    feat: :sparkles: 文書の追加 style: :lipstick: CSSの整理

--- pages/index.vue
+++ pages/index.vue
@@ -10,12 +10,22 @@
         <div class="section">
             <div class="section-title">
                 <h2>Researches</h2>
-                <p>研究&業績</p>
+                <p>最新の研究&業績</p>
             </div>
 
             <div class="section-content">
                 <div class="paper-img">
                     <NuxtImg src="/img/paper/Nanotechnology.webp" alt="paper" />
+                </div>
+                <div class="paper-container flex">
+                    <div class="paper">
+                        <div class="paper-title">
+                            <a href="https://iopscience.iop.org/article/10.1088/1361-6528/ab7fcf" target="_blank" rel="noopener noreferrer">Neutral oxygen irradiation enhanced forming-less ZnO-based transparent analog memristor devices for neuromorphic computing applications</a>
+                        </div>
+                        <div class="paper-author">Firman Mangasa Simanjuntak, Takeo Ohno, Sridhar Chandrasekaran, Tseung-Yuen Tseng and Seiji Samukawa</div>
+                        <div class="paper-publish">Nanotechnology 31, 26LT01 (2020).</div>
+                    </div>
+                    <div class="paper-content">酸素ビームを照射することで膜質が向上したZnO薄膜を持つ全透明アナログメモリスタ素子を試作した結果、未照射の素子と比較して繰り返しスイッチング回数が20倍に増加し、動作電流が10分の1に低減しました。また、この素子を用いてシナプス動作を再現し、その結果を用いて画像認識における精度向上に関する知見を得ました。</div>
                 </div>
             </div>
         </div>
@@ -24,6 +34,14 @@
             <div class="section-title">
                 <h2>Equipments</h2>
                 <p>研究設備</p>
+            </div>
+
+            <div class="equi-section">
+                <div class="equi-area">
+                    <div class="equi-section">
+                        <p class="equi-title"></p>
+                    </div>
+                </div>
             </div>
         </div>
 
