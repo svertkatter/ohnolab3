8555b5265b8818935c1aada297c454006a01bd1d
svertkatter <alice241102@gmail.com>
2024-08-08 21:27:22 +0900

    chore: :pencil2: ページの編集

--- pages/room/index.vue
+++ pages/room/index.vue
@@ -6,6 +6,69 @@
                 <p>研究室の様子</p>
             </div>
 
+            <div class="section-content">
+                <div class="section-room">
+                    <h3 class="blue-title">研究室の様子</h3>
+                    <div class="room-img">
+                        <NuxtImg src="/img/room/room01.webp"/>
+                    </div>
+                    <p>みなさんの研究を促進するために生活環境に気を遣っています。</p>
+                </div>
+            </div>
+
+            <div class="section-content">
+                <div class="section-room">
+                    <h3 class="blue-title">空気清浄機と温冷蔵庫も完備</h3>
+                    <div class="room-img-another flex">
+                        <NuxtImg src="/img/room/room02.webp"/>
+                        <NuxtImg src="/img/room/room04.webp"/>                        
+                    </div>
+                </div>
+            </div>
+
+            <div class="section-content">
+                <div class="section-room">
+                    <h3 class="blue-title">コーヒーメーカーで一息つきませんか？</h3>
+                    <div class="room-img">
+                        <NuxtImg src="/img/room/room05.webp"/>
+                    </div>
+                </div>
+            </div>
+
+            <div class="section-content">
+                <div class="section-room">
+                    <h3 class="blue-title">パソコンもご自由にどうぞ</h3>
+                    <div class="room-img">
+                        <NuxtImg src="/img/room/room03.webp"/>
+                    </div>
+                </div>
+            </div>
+
+            <div class="section-content">
+                <div class="section-room">
+                    <h3 class="blue-title">普段の様子</h3>
+                    <div class="room-img">
+                        <NuxtImg src="/img/room/room07.webp"/>
+                    </div>
+                    <div class="room-img">
+                        <NuxtImg src="/img/room/room08.webp"/>
+                    </div>
+                    <p>プロジェクターを壁に投影でき、みんなで熱心に議論します。</p>
+                </div>
+            </div>
+
+            <div class="section-content">
+                <div class="section-room">
+                    <h3 class="blue-title">春には桜が満開です</h3>
+                    <div class="room-img">
+                        <NuxtImg src="/img/room/room09.webp"/>
+                    </div>
+                    <div class="room-img">
+                        <NuxtImg src="/img/room/room10.webp"/>
+                    </div>
+                </div>
+            </div>
+
             <div class="button-back"><BackButton/></div>
 
             
