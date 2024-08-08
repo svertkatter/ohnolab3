14f8c46a3cb366c584f8142be35ce8de9aa76fab
svertkatter <alice241102@gmail.com>
2024-08-08 20:53:44 +0900

    chore: :pencil2: メンバーページの編集

---
+++ components/PapersLinkButton.vue
@@ -0,0 +1,16 @@
+<template>
+        <a class="paper-button" :href="link" target="_blank" rel="noopener noreferrer">{{ text }}</a>
+</template>
+
+<script setup>
+defineProps({
+    link: {
+        type: String,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    }
+})
+</script>
--- pages/members/index.vue
+++ pages/members/index.vue
@@ -1,2 +1,152 @@
 <template>
+    <article class="container member-article">
+        <div class="section">
+            <div class="section-title">
+                <h2>Member</h2>
+                <p>メンバー</p>
+            </div>
+
+
+            <div class="member-section">
+                <p class="blue-title">指導教員</p>
+                <div class="member-content">
+                    <p>准教授 / Associate Professor, PI</p>
+                    <p class="professor">大野 武雄　博士（工学） / Takeo Ohno, PhD in Materials Science</p>
+                    <p>大分大学理工学部理工学科電気エネルギー・電子工学プログラム
+                        （兼）大分大学大学院工学研究科工学選考電気電子工学コース</p>
+                </div>
+                <p class="member-content-en">Electrical Power and Electronics Program, Department of Science and Technology, Faculty of Science and Technology, Oita University (concurrently) Division of Electrical and Electronic Engineering, Department of Engineering, Graduate School of Engineering, Oita University</p>
+                <div class="link-button-area">
+                    <PapersLinkButton link="https://publons.com/researcher/2769392/takeo-ohno" text="publons"/>
+
+                    <PapersLinkButton link="https://www.scopus.com/authid/detail.uri?authorId=7402659251" text="Scopus"/>
+
+                    <PapersLinkButton link="https://scholar.google.com/citations?user=RSKRq7wAAAAJ&hl=EN" text="Google Scholar"/>
+
+                    <PapersLinkButton link="https://orcid.org/0000-0001-9990-5544" text="ORCiD"/>
+
+                    <PapersLinkButton link="https://jglobal.jst.go.jp/detail/?JGLOBAL_ID=200901012863419430" text="J-GLOBAL"/>
+
+                    <PapersLinkButton link="https://nrid.nii.ac.jp/ja/nrid/1000090447144/" text="KAKEN"/>
+
+                    <PapersLinkButton link="https://researchmap.jp/read0103300" text="researchmap"/>
+                </div>
+            </div>
+
+            <div class="member-section">
+                <p class="blue-title">現在のメンバー</p>
+                <div class="member-content">
+                    <p>技術補佐員 / Technical Assistant</p>
+                    <p class="professor">椎原理恵 / Rie Shiihara</p>
+                </div>
+
+                <div class="member-content margin-top">
+                    <p>2023年度の学生</p>
+                    <div class="member-area">
+                        <div>
+                            <p>MC2</p>
+                            <p class="professor">G. Takashita</p>
+                        </div>
+        
+                        <div>
+                            <p>MC1</p>
+                            <p class="professor">G. Takashita</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="member-section">
+                <p class="blue-title">これまでのメンバーと卒業後の進路</p>
+
+                <div class="member-content margin-top">
+                    <p>2022年度の修了生・卒業生と進路</p>
+                    <div class="member-area">
+                            <div>
+                                <p>MC</p>
+                                <p class="professor">K.Sakamoto, Y.Sato, K.Minami</p>
+                            </div>
+            
+                            <div>
+                                <p>BC</p>
+                                <p class="professor">Y.Saigo, K.Eto, R.Tonai, K.Hamada, M.Harada, T.Inoue</p>
+                            </div>
+                    </div>
+                    <div>
+                        <p>進路</p>
+                        <p class="professor">I-O DATA DEVICE、アイリスオーヤマ、大分大学大学院進学、九州工業大学大学院入学、ソニーセミコンダクタマニュファクチャリング、前川製作所</p>
+                    </div>
+                </div>
+
+                <div class="member-content margin-top">
+                    <p>2021年度の修了生・卒業生と進路</p>
+                    <div class="member-area">
+                            <div>
+                                <p>MC</p>
+                                <p class="professor">N. Katafuchi, S. Mieno</p>
+                            </div>
+            
+                            <div>
+                                <p>BC</p>
+                                <p class="professor">K. Uchida, T. Shirakami, G. Takashita, T. Taki, K. Hamada, R. Matsubara, H. Nagatomo</p>
+                            </div>
+                    </div>
+                    <div>
+                        <p>進路</p>
+                        <p class="professor">大分市役所、大分大学大学院進学、山久、三精テクノロジーズ、JR東日本情報システム、日産車体、日本マイクロニクス、浜松ホトニクス、ホンダテクノフォート</p>
+                    </div>
+                </div>
+
+                <div class="member-content margin-top">
+                    <p>2020年度の修了生・卒業生と進路</p>
+                    <div class="member-area">
+                        <div>
+                            <p>MC</p>
+                            <p class="professor">I. Obata, K. Kamei, S. Ninomiya</p>
+                        </div>
+        
+                        <div>
+                            <p>BC</p>
+                            <p class="professor">K. Sakamoto, Y. Sato, K. Fujii, K. Minami, H. Sato</p>
+                        </div>
+                    </div>
+                    <div>
+                        <p>進路</p>
+                        <p class="professor">石井工作研究所、大分大学大学院進学、トヨタカローラ大分、ニチコン、三菱電機エンジニアリング、ローム</p>
+                    </div>
+                </div>
+
+                <div class="member-content margin-top">
+                    <p>2019年度の修了生・卒業生と進路</p>
+                    <div>
+                        <div>
+                            <p>BC</p>
+                            <p class="professor">J. Kai, N. Katafuchi, E. Higashi, T. Furuno, S. Mieno, R. Furutani</p>
+                        </div>
+                    </div>
+                    <div>
+                        <p>進路</p>
+                        <p class="professor">大分キヤノンマテリアル、大分大学大学院進学、JFEプラントエンジ、デンケン、メルコセミコンダクタエンジニアリング</p>
+                    </div>
+                </div>
+
+                <div class="member-content margin-top">
+                    <p>2018年度の修了生・卒業生と進路</p>
+                    <div>
+                        <div>
+                            <p>BC</p>
+                            <p class="professor">Y. Anai, K. Abe, I. Obata, K. Kamei, S. Ninomiya, R. Takano</p>
+                        </div>
+                    </div>
+                    <div>
+                        <p>進路</p>
+                        <p class="professor">大分大学大学院進学、八幡浜市役所、パーソルR&D</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="button-back"><BackButton/></div>
+
+        </div>
+    </article>
 </template>
