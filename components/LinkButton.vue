51f4a8eb6902e93b79ee383ccdd24fd9a631a95e
svertkatter <alice241102@gmail.com>
2024-08-08 22:13:22 +0900

    style: :lipstick: 調整

--- components/LinkButton.vue
+++ components/LinkButton.vue
@@ -1,4 +1,41 @@
 <template>
+        <!-- 内部リンクの場合 -->
+        <NuxtLink v-if="isInternalLink" :to="link" class="paper-button" target="_blank" rel="noopener noreferrer">
+            <NuxtImg v-if="icon" :src="icon" alt="icon" class="icon" />
+            {{ text }}
+        </NuxtLink>
+
+        <!-- 外部リンクの場合 -->
+        <a v-else :href="link" class="paper-button" target="_blank" rel="noopener noreferrer">
+            <NuxtImg v-if="icon" :src="icon" alt="icon" class="icon" />
+            {{ text }}
+        </a>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+
+// プロパティの定義
+const props = defineProps({
+    link: {
+        type: String,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    icon: {
+        type: String,
+        required: false
+    }
+})
+
+// 内部リンクか外部リンクかを判定
+const isInternalLink = computed(() => props.link.startsWith('/'))
+</script>
+
+<!-- <template>
         <a class="paper-button" :href="link" target="_blank" rel="noopener noreferrer">
             <NuxtImg v-if="icon" :src="icon"/>
             {{ text }}
@@ -20,4 +57,4 @@ defineProps({
         required: true
     }
 })
-</script>
+</script> -->
---
+++ pages/form/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <article class="container form-article">
+        <div class="section">
+            <div class="section-title">
+                <h2>Form</h2>
+                <p>お問い合わせフォーム</p>
+            </div>
+
+            <div class="button-back"><BackButton/></div>
+
+            
+        </div>
+    </article>
+</template>
