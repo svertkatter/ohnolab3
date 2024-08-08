f10e09ed8f2a7c7bf0f995acb25fceeed1dfd8b6
svertkatter <alice241102@gmail.com>
2024-08-08 17:18:57 +0900

    chore: :pencil2: researchesのページを編集

---
+++ components/PapersCard.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="section-content">
+                <div class="paper-img">
+                    <NuxtImg :src="imageSrc" alt="paper" />
+                </div>
+                <div class="paper-container flex">
+                    <div class="paper">
+                        <div class="paper-title">
+                            <a :href="link" target="_blank" rel="noopener noreferrer">{{ title }}</a>
+                        </div>
+                        <div class="paper-author">{{ author }}</div>
+                        <div class="paper-publish">{{ publish }}</div>
+                    </div>
+                    <div class="paper-content">{{ content }}</div>
+                </div>
+            </div>
+</template>
+
+<script setup>
+defineProps({
+    imageSrc: {
+    type: String,
+    required: true
+  },
+link:{
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  author:{
+    type: String,
+    required: true
+  },
+  publish:{
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+})
+</script>
--- pages/researches/index.vue
+++ pages/researches/index.vue
@@ -5,6 +5,17 @@
                 <h2>Researches</h2>
                 <p>最新の研究&業績</p>
             </div>
+
+            <PapersCard
+            imageSrc="/img/paper/Nanotechnology.webp"
+            link="https://iopscience.iop.org/article/10.1088/1361-6528/ab7fcf"
+            title="Neutral oxygen irradiation enhanced forming-less ZnO-based transparent analog memristor devices for neuromorphic computing applications"
+            author="Firman Mangasa Simanjuntak, Takeo Ohno, Sridhar Chandrasekaran, Tseung-Yuen Tseng and Seiji Samukawa"
+            publish="Nanotechnology 31, 26LT01 (2020)."
+            content="酸素ビームを照射することで膜質が向上したZnO薄膜を持つ全透明アナログメモリスタ素子を試作した結果、未照射の素子と比較して繰り返しスイッチング回数が20倍に増加し、動作電流が10分の1に低減しました。また、この素子を用いてシナプス動作を再現し、その結果を用いて画像認識における精度向上に関する知見を得ました。"
+            />
+
+            
         </div>
     </article>
 </template>
