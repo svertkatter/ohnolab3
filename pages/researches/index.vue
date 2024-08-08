4068d3972719d71fc8cc26494652ca96f0c8c786
svertkatter <alice241102@gmail.com>
2024-08-08 17:50:47 +0900

    style: :lipstick: 画像の調整

--- pages/researches/index.vue
+++ pages/researches/index.vue
@@ -15,6 +15,15 @@
             content="酸素ビームを照射することで膜質が向上したZnO薄膜を持つ全透明アナログメモリスタ素子を試作した結果、未照射の素子と比較して繰り返しスイッチング回数が20倍に増加し、動作電流が10分の1に低減しました。また、この素子を用いてシナプス動作を再現し、その結果を用いて画像認識における精度向上に関する知見を得ました。"
             />
 
+            <PapersCard
+            imageSrc="/img/paper/z01.webp"
+            link="https://advances.sciencemag.org/content/4/2/eaar2250"
+            title="Current-induced magnetization switching using an electrically insulating spin-torque generator"
+            author="Hongyu An, Takeo Ohno, Yusuke Kanno, Yuito Kageyama, Yasuaki Monnai, Hideyuki Maki, Ji Shi, and Kazuya Ando"
+            publish="Nanotechnology 31, 26LT01 (2020)."
+            content="空間反転対称性の破れを用いることで、金属酸化物絶縁体を用いたスピン軌道トルク生成と 磁化制御を実現しました。さらに、金属酸化物中の酸素を電場により移動させることで、 強磁性金属/金属酸化物界面におけるスピン軌道トルクの不揮発な電気的制御に成功しました。"
+            />
+
             
         </div>
     </article>
