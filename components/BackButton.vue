f4e4658b8438bd05e0d8032fd2ad2a5b98918b34
svertkatter <alice241102@gmail.com>
2024-08-08 18:24:49 +0900

    feat: :sparkles: 戻るボタンを追加

---
+++ components/BackButton.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="moreButton">
+      <NuxtLink to="/" class="buttonLink">
+        <span class="arrowLeft"></span>
+        <p>Back</p>
+      </NuxtLink>
+    </div>
+  </template>
